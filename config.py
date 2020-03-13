fcf1c964cb3532a219173ab1ae4c592e6ecf7c30
musab <musabalfarizi@gmail.com>
2020-03-13 12:43:15 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,40 @@
+# config.py
+
+class Config(object):
+    """
+    Common configurations
+    """
+    DEBUG = True
+
+    # Put any configurations here that are common across all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
+
